7ebd8206dc816d0adffdc37dc48d81330ecf5857
greenfest <1greenfest@gmail.com>
2024-02-08 23:33:52 +0500

    added unit-tests-3

---
+++ unit-tests-3/Test/NumberUtilsTest.java
@@ -0,0 +1,41 @@
+﻿import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NumberUtilsTest {
+
+    @Test
+    void testEvenNumber() {
+        YourClass yourObject = new YourClass();
+        boolean result = yourObject.evenOddNumber(4);
+        assertTrue(result);
+    }
+
+    @Test
+    void testOddNumber() {
+        YourClass yourObject = new YourClass();
+        boolean result = yourObject.evenOddNumber(7);
+        assertFalse(result);
+    }
+
+    @Test
+    void testNumberInInterval() {
+        YourClass yourObject = new YourClass();
+        boolean result = yourObject.isNumberInInterval(50);
+        assertTrue(result);
+    }
+
+    @Test
+    void testNumberNotInInterval() {
+        YourClass yourObject = new YourClass();
+        boolean result = yourObject.isNumberInInterval(120);
+        assertFalse(result);
+    }
+
+    @Test
+    void testNumberOnIntervalBoundary() {
+        YourClass yourObject = new YourClass();
+        boolean result = yourObject.isNumberInInterval(25);
+        assertFalse(result);
+    }
+}
